54f1542d34a4b06bd94a53590b62f33f459d2b7a
John-code10 <jhon.alvarez.ulate@gmail.com>
2025-02-21 19:02:16 -0600

    .

--- cypress/e2e/LocalizadoresURL.cy.js
+++ cypress/e2e/LocalizadoresURL.cy.js
@@ -5,10 +5,12 @@ beforeEach(() => {
 })
 describe('Verificación de la URL en la página de ejemplo de Cypress', () => {
     it('Debería cargar la página correctamente y validar la URL', () => {
+        //cy.title().should('eq', 'Welcome to Cypress')
         cy.location('pathname').should('eq', '/')
         cy.location('hostname').should('eq', 'example.cypress.io') 
         cy.location('protocol').should('eq', 'https:')
         cy.location('href').should('eq', 'https://example.cypress.io/')
         cy.location('host').should('eq', 'example.cypress.io')
+        cy.url().should('eq', 'https://example.cypress.io/')
     })
 })
